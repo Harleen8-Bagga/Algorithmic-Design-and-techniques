ba90ca7d214daab3ccbebf5c58dec591333c69a8
Harleen8Kaur1998 <53442613+Harleen8Kaur1998@users.noreply.github.com>
2019-09-21 05:03:41 +0530

    Add files via upload

---
+++ gcd.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+long long int euclid(long long int a, long long int b)
+{
+	if (b == 0)
+		return(a);
+	else if (b == 1)
+		return(b);
+	else
+	{
+		return(euclid(b, a%b));
+
+	}
+}
+int main()
+{
+	long long int a, b;
+	cin >> a;
+	cin >> b;
+	long long int c = euclid(a, b);
+	cout << c;
+	return(0);
+
+
+}
